ce64b5a50c02a8a3736810db7d373d3ca0f62498
Augusto Albertoni <augusto.albertoni@gmail.com>
2019-02-24 22:03:30 -0300

    Session, signin, middlewares

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const path = require('path')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('connect-flash')
+
 class App {
-  constructor () {
+  constructor() {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
@@ -11,11 +15,21 @@ class App {
     this.routes()
   }
 
-  middlewares () {
+  middlewares() {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(flash())
+    this.express.use(session({
+      name: 'root',
+      secret: 'MyAppSecret',
+      resave: true,
+      store: new FileStore({
+        path: path.resolve(__dirname, '..', 'tmp', 'sessions')
+      }),
+      saveUninitialized: true
+    }))
   }
 
-  views () {
+  views() {
     nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
       watch: this.isDev,
       express: this.express,
@@ -27,7 +41,7 @@ class App {
     this.express.set('view engine', 'njk')
   }
 
-  routes () {
+  routes() {
     this.express.use(require('./routes'))
   }
 }
